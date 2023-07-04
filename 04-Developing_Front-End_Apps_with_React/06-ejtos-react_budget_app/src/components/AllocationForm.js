a89f4b7e09bdbdcd981bc433c5b010fc19c11522
Starboy <samskywalker001@gmail.com>
2023-07-05 03:28:47 +0530

    July05

--- 04-Developing_Front-End_Apps_with_React/06-ejtos-react_budget_app/src/components/AllocationForm.js
+++ 04-Developing_Front-End_Apps_with_React/06-ejtos-react_budget_app/src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch, remaining, currency } = useContext(AppContext);
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
     const [action, setAction] = useState('');
@@ -10,7 +10,7 @@ const AllocationForm = (props) => {
     const submitEvent = () => {
 
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
+                alert("The value cannot exceed remaining funds: " + currency + remaining);
                 setCost("");
                 return;
             }
@@ -58,14 +58,17 @@ const AllocationForm = (props) => {
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
-                    <input
-                        required='required'
-                        type='number'
-                        id='cost'
-                        value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
-                        onChange={(event) => setCost(event.target.value)}>
-                        </input>
+                    <div style={{marginLeft: '2rem' }}>
+                        <span style={{fontSize: '1.2rem'}}>{currency}</span>
+                        <input
+                            required='required'
+                            type='number'
+                            id='cost'
+                            value={cost}
+                            style={{ marginLeft: '2rem' , size: 10}}
+                            onChange={(event) => setCost(event.target.value)}>
+                        </input> 
+                    </div>
 
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
--- 04-Developing_Front-End_Apps_with_React/06-ejtos-react_budget_app/src/components/Budget.js
+++ 04-Developing_Front-End_Apps_with_React/06-ejtos-react_budget_app/src/components/Budget.js
@@ -2,11 +2,32 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch, currency, remaining } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const handleEditBudget = (e)=>{
+        if (e.target.value > 20000) {
+            alert("The value can't exceed remaining funds "+ currency + remaining );
+            console.log(budget)
+            console.log("The value can't exceed remaining funds "+ currency + remaining );
+        }
+        else if (e.target.value < totalExpenses) {
+            alert("You can't reduce the budget value lower than the spending");
+            console.log("You can't reduce the budget value lower than the spending");
+        }
+        else {
+            dispatch({type: "SET_BUDGET", payload: e.target.value});       
+        }
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget {currency}</span><input type="number" step={10} value={budget} onChange={handleEditBudget}/>
         </div>
     );
 };
+
 export default Budget;
